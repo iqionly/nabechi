aec09672a22ea4ed54621f1118c5c7ae9a04643d
izzy25 <izzy25.mr@gmail.com>
2021-10-27 23:27:32 +0700

    master

---
+++ src/components/CustomBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class CustomBar extends React.Component {
+    
+    render() {
+        return(
+            <React.Fragement>
+                This is Bar
+            </React.Fragement>
+        );
+    }
+
+}
+
+export default CustomBar;
